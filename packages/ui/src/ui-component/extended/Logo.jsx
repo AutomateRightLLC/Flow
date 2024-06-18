abf62d4238b406111e8dd20755d102801ed21c1d
Kyle Bebeau <kyle@theinitgroup.com>
2024-06-18 14:24:12 -0400

    theme flowise for demo purposes

--- packages/ui/src/ui-component/extended/Logo.jsx
+++ packages/ui/src/ui-component/extended/Logo.jsx
@@ -1,5 +1,5 @@
-import logo from '@/assets/images/flowise_logo.png'
-import logoDark from '@/assets/images/flowise_logo_dark.png'
+import logo from '@/assets/images/dark-logo.svg'
+import logoDark from '@/assets/images/light-logo.svg'
 
 import { useSelector } from 'react-redux'
 
